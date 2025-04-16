b42e55f8d33a30ace1b1ca34c4623830725f0a5c
Smit Patel <113251493+smit0308@users.noreply.github.com>
2025-04-16 14:39:14 +0530

    Update notificationRoutes.js

--- Backend/routes/notificationRoutes.js
+++ Backend/routes/notificationRoutes.js
@@ -7,7 +7,7 @@ const {
   markAllAsRead,
   getUnreadCount,
 } = require("../controllers/notificationController");
-const { protect } = require("../middleWare/authMiddleware");
+const { protect } = require("../middleWare/authMiddleWare");
 
 router.post("/", protect, createNotification);
 router.get("/", protect, getNotifications);
